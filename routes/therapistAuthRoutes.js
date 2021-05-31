750a00822ed73210ddb99c86d0eb4ce32231b91c
Ali Sanad <alisanad099@gmail.com>
2021-06-01 00:18:48 +0200

    fix cloudinary bug

--- routes/therapistAuthRoutes.js
+++ routes/therapistAuthRoutes.js
@@ -1,20 +1,20 @@
-const { Router } = require("express");
+const {Router} = require('express');
 const router = Router();
-const therapistAuthController = require("../controller/therapistAuthController");
-const therapistProfileController = require("../controller/therapistProfileController");
+const therapistAuthController = require('../controller/therapistAuthController');
+const therapistProfileController = require('../controller/therapistProfileController');
 const {
   requireAuth,
   checkUser,
-} = require("./../middlewares/therapistAuthMiddleware");
-router.get("/signup", therapistAuthController.signup_get);
-router.post("/signup", therapistAuthController.signup_post);
-router.get("/login", therapistAuthController.login_get);
-router.post("/login", therapistAuthController.login_post);
-router.get("/logout", therapistAuthController.logout_get);
+} = require('./../middlewares/therapistAuthMiddleware');
+// router.get("/signup", therapistAuthController.signup_get);
+router.post('/signup', therapistAuthController.signup_post);
+// router.get('/login', therapistAuthController.login_get);
+router.post('/login', therapistAuthController.login_post);
+router.get('/logout', therapistAuthController.logout_get);
 
-router.get("/", therapistAuthController.getAllTherapists);
-router.get("/:id", therapistAuthController.getOneTherapist);
-router.patch("/updatatherapist/:id", therapistAuthController.updataTherapist);
-router.delete("/deletetherapist/:id", therapistAuthController.deleteTherapist);
+router.get('/', therapistAuthController.getAllTherapists);
+router.get('/:id', therapistAuthController.getOneTherapist);
+router.patch('/updatatherapist/:id', therapistAuthController.updataTherapist);
+router.delete('/deletetherapist/:id', therapistAuthController.deleteTherapist);
 
 module.exports = router;
