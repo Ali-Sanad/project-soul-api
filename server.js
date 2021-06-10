c03231de19ca283a4b3915d64e4ff877bffa3907
Ali Sanad <alisanad099@gmail.com>
2021-06-11 01:23:52 +0200

    up

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true}));
 
-app.use('/', (req, res) => res.json({status: 'API is running'}));
+app.get('/', (req, res) => res.json({status: 'API is running'}));
 
 //@define routes
 app.use('/api/auth', require('./routes/auth'));
