b2ecc35a397a28d83d294256a0199e09b8660efe
Ali Sanad <alisanad099@gmail.com>
2021-06-11 09:44:02 +0200

    fixing bug

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true}));
 
-app.get('/', (req, res) => res.send({status: 'API is running'}));
+app.get('/test', (req, res) => {
+  res.send({status: 'API is running'});
+});
 
 //@define routes
 app.use('/api/auth', require('./routes/auth'));
