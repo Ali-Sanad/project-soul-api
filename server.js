9043c7e690a81fcf0301769c7de6024e973a215b
Ali Sanad <alisanad099@gmail.com>
2021-06-11 09:38:36 +0200

    fixing bug

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
-const path = require('path');
+
 const app = express();
 //connect database
 connectDB();
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true}));
 
-app.get('/', (req, res) => res.json({status: 'API is running'}));
+app.get('/', (req, res) => res.send({status: 'API is running'}));
 
 //@define routes
 app.use('/api/auth', require('./routes/auth'));
