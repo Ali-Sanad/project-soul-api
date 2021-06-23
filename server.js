700e1becf1d000d7756039088f80a7ea3ff68699
HadeerOmar <hadeeromarfci@gmail.com>
2021-06-24 00:00:37 +0200

    video localhost:3030

--- server.js
+++ server.js
@@ -1,11 +1,44 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const cors = require('cors');
-
 const app = express();
+const app2 = express();
+
+const cors = require('cors');
+const server = require("http").Server(app2);
+const { v4: uuidv4 } = require("uuid");
+app2.set("view engine", "ejs");
+const io = require("socket.io")(server, {
+  cors: {
+    origin: '*'
+  }
+});
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
 //connect database
 connectDB();
 
+app2.use("/peerjs", peerServer);
+app2.use(express.static("public"));
+
+app2.get("/", (req, res) => {
+  res.redirect(`/${uuidv4()}`);
+});
+
+app2.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId, userName) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message, userName);
+    });
+  });
+});
 //enable cors for all routes
 app.use(cors());
 
@@ -41,7 +74,10 @@ app.use('/api/posts', require('./routes/posts'));
 // }
 
 const PORT = process.env.PORT || 5000;
+const PORT2 = process.env.PORT2 || 3030;
+
 
 app.listen(PORT, () => {
   console.log(`Server started on port:${PORT}`);
 });
+server.listen(process.env.PORT2 || 3030);
