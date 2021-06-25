b901de78acea82f47f31a5e4af7700f046992bec
yasmin <yasminfathy520@gmail.com>
2021-06-25 22:08:44 +0200

    fix-therapist-model

--- models/TherapistModel.js
+++ models/TherapistModel.js
@@ -47,6 +47,7 @@ const TherapistSchema = new mongoose.Schema(
     },
     fees: {
       type: Number,
+      default: 150,
     },
     // passwordCgangedAt: Date,
     passwordResetToken: String,
