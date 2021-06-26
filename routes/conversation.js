37140e0311e52c50dd89bf60a72bc5d23b64f3a4
ManarSayedEid <manarsayed1994@gmail.com>
2021-06-26 02:01:19 +0200

    sort chat members descending

--- routes/conversation.js
+++ routes/conversation.js
@@ -19,7 +19,7 @@ router.get("/:userId", async (req, res) => {
   try {
     const conversations = await Conversation.find({
       members: { $in: [req.params.userId] },
-    });
+    }).sort({ date: "desc" });
     res.status(200).json(conversations);
   } catch (err) {
     res.status(500).json(err);
