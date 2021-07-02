9ac1b9689b97ceb3ad21c05dae21df3613c4a1e3
Ali Sanad <alisanad099@gmail.com>
2021-07-02 15:59:20 +0200

    fix

--- server.js
+++ server.js
@@ -2,44 +2,25 @@ const express = require('express');
 const path = require('path');
 const connectDB = require('./config/db');
 const app = express();
-const app2 = express();
 
 const cors = require('cors');
-const server = require('http').Server(app2);
 const {v4: uuidv4} = require('uuid');
-app2.set('view engine', 'ejs');
+
 const io = require('socket.io')(server, {
   cors: {
     origin: '*',
   },
 });
-const {ExpressPeerServer} = require('peer');
-const peerServer = ExpressPeerServer(server, {
-  debug: true,
-});
+
 //connect database
 connectDB();
 
 //enable cors for all routes
 app.use(cors());
-//upload image
 // app.use(express.static('public'));
-// app.use(express.json({ limit: '50mb' }));
-// app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.get('/', (req, res) => {
   res.send({status: 'API is running'});
-});
-
-app2.use('/peerjs', peerServer);
-app2.use(express.static('public'));
-
-app2.get('/', (req, res) => {
-  res.redirect(`/${uuidv4()}`);
-});
-
-app2.get('/:room', (req, res) => {
-  res.render('room', {roomId: req.params.room});
 });
 
 io.on('connection', (socket) => {
@@ -51,6 +32,7 @@ io.on('connection', (socket) => {
     });
   });
 });
+
 //Init middleware for bodyparser
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true}));
@@ -82,9 +64,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const PORT = process.env.PORT || 5000;
-const PORT2 = process.env.PORT2 || 3030;
 
 app.listen(PORT, () => {
   console.log(`Server started on port:${PORT}`);
 });
-server.listen(process.env.PORT2 || 3030);
