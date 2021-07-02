530423ba35522e7c2117909fd4b27eb05a90190b
Ali Sanad <alisanad099@gmail.com>
2021-07-02 15:46:15 +0200

    fix

--- server.js
+++ server.js
@@ -27,9 +27,9 @@ app.use(cors());
 // app.use(express.json({ limit: '50mb' }));
 // app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-// app.get('/', (req, res) => {
-//   res.send({status: 'API is running'});
-// });
+app.get('/', (req, res) => {
+  res.send({status: 'API is running'});
+});
 
 app2.use('/peerjs', peerServer);
 app2.use(express.static('public'));
